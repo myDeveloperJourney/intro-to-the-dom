9d7f81f26169670c6be034e057b86ed7cedf8a79
Daniel JS <daniel@mydeveloperjourney.com>
2022-08-05 12:24:50 -0500

    wip: selects dom element

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert('JavaScript is linked!');
+// Let's select an element using JavaScript and the (document) DOM API
+
+/* 
+    try selecting the h1 element using JavaScript and assigning it to a variable
+    named h1Element then console.log the variable
+*/
+
+const h1Element = document.getElementById('title');
+console.log(h1Element);
+h1Element.style.color = 'red';
