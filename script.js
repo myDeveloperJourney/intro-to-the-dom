bf923a7a64d68982a9230fe509a435eb4ba42ea2
Daniel JS <daniel@mydeveloperjourney.com>
2022-08-05 13:01:26 -0500

    wip

--- script.js
+++ script.js
@@ -7,13 +7,29 @@
 
 // select an element using getElementById
 // const h1Element = document.getElementById('title');
-// h1Element.style.color = 'red';
+
 
 // select an element using querySelector
 // works like a css selector
-const h1Element = document.querySelector('.main-title-large');
+const h1Element = document.querySelector('.main-title');
 
 // console.log(h1Element);
 
+// selecting a p tag
 const pEl = document.querySelector('.cool');
-console.log(pEl);
+
+// how to change the content of a DOM element
+
+// innerHTML - used for setting text and html
+
+pEl.innerHTML = "Comments for <strong>today</strong>";
+
+// textContent = used for setting text only
+
+// pEl.textContent = "Comments for <strong>today</strong>";
+
+// Changing css styles of an element using JavaScript
+
+h1Element.style.color = 'red';
+h1Element.style.fontFamily = 'Helvetica';
+h1Element.style.textAlign = 'center';
